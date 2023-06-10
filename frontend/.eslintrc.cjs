e0e5642d54a3f55e558f380d4e98e592e439e959
nkwaaaa <nkwaaaa@gmail.com>
2023-06-10 19:25:19 -0300

    add routes

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,25 +5,30 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:react/jsx-runtime'
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:react/jsx-runtime"
   ],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
     tsconfigRootDir: __dirname
 
   },
   plugins: [
-    'react'
+    "react"
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/space-before-function-paren': 0
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/space-before-function-paren": 0,
+    "@typescript-eslint/quotes": 0,
+    quotes: ["error", "double"],
+    "@typescript-eslint/no-unused-vars": 1
+    /* '@typescript-eslint/promise-function-async': 0,
+    '@typescript-eslint/no-floating-promises': 0 */
 
   }
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Signup from "./routes/Signup.tsx"
+import Dashboard from "./routes/Dashboard.tsx"
+import ProtectedRoute from "./routes/ProtectedRoute.tsx"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
