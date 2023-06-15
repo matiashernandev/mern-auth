41b7edbb3d004a007b95a7f4a8a0260b5bc25abb
nkwaaaa <nkwaaaa@gmail.com>
2023-06-15 11:40:36 -0300

    add mongo connection with atlas

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,7 @@ import signoutRouter from './routes/signout.js';
 import signupRouter from './routes/signup.js';
 import userRouter from './routes/user.js';
 import todosRouter from './routes/todos.js';
+
 config()
 
 const port = process.env.PORT || 5000
@@ -16,6 +17,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+async function main() {
+  await mongoose.connect(process.env.BD_CONNECTION_STRING)
+  console.log("Connected to mongodb")
+}
+
+main().catch(console.error)
 
 app.use("/api/signout", signoutRouter)
 app.use('/api/signup', signupRouter);
