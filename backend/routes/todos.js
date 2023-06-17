ac0ba6dea6b7031c0035153c7e9a86fffb508996
nkwaaaa <nkwaaaa@gmail.com>
2023-06-17 12:10:12 -0300

    add todos front

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,9 +1,43 @@
 import { Router } from "express";
+import Todo from "../schema/todo.js";
 
 const router = Router()
 
 router.get("/", (req, res) => {
-  res.send("todos")
+  res.json([{
+    id: 1,
+    title: "Hernán",
+    completed: false
+  }])
+})
+
+
+router.post("/", async (req, res) => {
+  if (!req.body.title) {
+    res.status(400).json({ error: "Title is required" })
+  }
+
+  try {
+    const todo = new Todo({
+      title: req.body.title,
+      completed: false,
+      idUser: req.user.id
+    })
+
+    const newTodo = await todo.save()
+
+    res.json(newTodo)
+
+  } catch (error) {
+
+  }
+
+  /* 
+    res.json([{
+      id: 1,
+      title: "Hernán",
+      completed: false
+    }]) */
 })
 
 export default router
--- frontend/src/routes/Dashboard.tsx
+++ frontend/src/routes/Dashboard.tsx
@@ -1,10 +1,89 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { useEffect, useState } from "react"
 import { useAuth } from "../auth/AuthProvider"
+import { API_URL } from "../auth/constants"
+
+interface Todos {
+  id: string
+  title: string
+  completed: boolean
+}
 
 export default function Dashboard() {
   const auth = useAuth()
 
+  const [todos, setTodos] = useState<Todos[]>([])
+  const [title, setTitle] = useState("")
+
+  async function loadTodos() {
+    try {
+      const response = await fetch(`${API_URL}todos`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.getAccessToken()}`
+        }
+      })
+
+      if (response.ok) {
+        const data = await response.json()
+        setTodos(data)
+      }
+    } catch (error) {
+
+    }
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    createTodo()
+  }
+  async function createTodo() {
+    try {
+      const response = await fetch(`${API_URL}todos`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+          Authorization: `Bearer ${auth.getAccessToken()}`
+        },
+        body: JSON.stringify({
+          title
+        })
+
+      })
+
+      if (response.ok) {
+        const json = await response.json()
+        setTodos([json, ...todos])
+      } else {
+
+        // mostrar error de conexión
+      }
+    } catch (error) {
+
+    }
+  }
+
+  useEffect(() => {
+    loadTodos()
+  }, [])
+
   return (
-    <div>Dashboard {auth.getUser()?.name ?? ""}</div>
+    <div>
+      <div>Dashboard {auth.getUser()?.name ?? ""}</div>
+
+      <form onSubmit={handleSubmit} >
+        <input type="text" placeholder="placeholder" onChange={(e) => setTitle(e.target.value)} value={title} />
+      </form>
+
+      {todos.map((todo) => (
+        <div key={todo.id} >{todo.title}</div>
+      ))}
+
+    </div>
   )
 }
